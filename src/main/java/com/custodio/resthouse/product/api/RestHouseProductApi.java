ddd5ebe48a0df453fcd2202130f6d9421b22735e
William Custodio <william@pedrinicustodio.com>
2019-09-03 15:38:23 +0200

    [RS-006] Removed unnecessary dependencies.

--- src/main/java/com/custodio/resthouse/product/api/RestHouseProductApi.java
+++ src/main/java/com/custodio/resthouse/product/api/RestHouseProductApi.java
@@ -2,9 +2,7 @@ package com.custodio.resthouse.product.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jms.annotation.EnableJms;
 
-@EnableJms
 @SpringBootApplication
 public class RestHouseProductApi
 {
